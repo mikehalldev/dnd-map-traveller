bda4b063f0d6d507b7c37a949ac767d6b2a811af
Michael Hall <mike@dynamometrics.com>
2020-01-12 14:36:15 -0500

    feat: Change transparency of GM image on click

--- Assets/ImageHandler.cs
+++ Assets/ImageHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ImageHandler : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class ImageHandler : MonoBehaviour
     private Canvas gmCanvas;
     private Canvas playerCanvas;
 
+    private EventTrigger.Entry clickEntry;
+
     void Start()
     {
         gmCanvas = GameObject.Find("Game Manager").GetComponentInChildren<Canvas>();
@@ -17,11 +20,41 @@ public class ImageHandler : MonoBehaviour
 
         images = gmCanvas.GetComponentsInChildren<Image>();
 
+        clickEntry = new EventTrigger.Entry();
+        clickEntry.eventID = EventTriggerType.PointerClick;
+        clickEntry.callback.AddListener((eventData) => { OnClick((PointerEventData) eventData); });
+
         foreach (Image image in images)
         {
+            EventTrigger trigger = image.gameObject.AddComponent<EventTrigger>();
+            trigger.triggers.Add(clickEntry);
+
             Image playerImage = Instantiate(image, image.transform.position, image.transform.rotation);
             playerImage.transform.SetParent(playerCanvas.transform);
             playerImage.transform.localScale = image.transform.localScale;
         }
+
+    }
+
+    private void OnClick(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerPress.name);
+        Image image = eventData.pointerPress.GetComponent<Image>();
+        ChangeTransparency(image);
+    }
+
+    private void ChangeTransparency(Image image)
+    {
+        Debug.Log("clicked!");
+        Color color = image.color;
+        if (color.a == 1)
+        {
+            color.a = 0;
+        }
+        else
+        {
+            color.a = 1;
+        }
+        image.color = color;
     }
 }
