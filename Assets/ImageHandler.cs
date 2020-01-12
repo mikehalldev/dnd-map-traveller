a9e638f3d16c2d5c5e844ffcfe8807434af30d28
Michael Hall <mike@dynamometrics.com>
2020-01-12 13:44:27 -0500

    feat: Reflect images on two displays

--- Assets/ImageHandler.cs
+++ Assets/ImageHandler.cs
@@ -6,38 +6,22 @@ using UnityEngine.UI;
 public class ImageHandler : MonoBehaviour
 {
 
-    private Dictionary<Image, Image> images;
+    private Image[] images;
     private Canvas gmCanvas;
     private Canvas playerCanvas;
 
     void Start()
     {
-        images = new Dictionary<Image, Image>();
         gmCanvas = GameObject.Find("Game Manager").GetComponentInChildren<Canvas>();
         playerCanvas = GameObject.Find("Player").GetComponentInChildren<Canvas>();
 
-        MakeImageDictionary();
-    }
+        images = gmCanvas.GetComponentsInChildren<Image>();
 
-    void MakeImageDictionary()
-    {
-        Image[] gmImages = gmCanvas.GetComponentsInChildren<Image>();
-        foreach (Image image in gmImages)
+        foreach (Image image in images)
         {
-            Image playerImage = MakePlayerImage(image);
-            images.Add(image, playerImage);
+            Image playerImage = Instantiate(image, image.transform.position, image.transform.rotation);
+            playerImage.transform.SetParent(playerCanvas.transform);
+            playerImage.transform.localScale = image.transform.localScale;
         }
-    }
-
-    Image MakePlayerImage(Image image)
-    {
-        Image playerImage = Instantiate(image, image.transform.position, image.transform.rotation);
-        playerImage.transform.SetParent(playerCanvas.transform);
-        playerImage.transform.localScale = image.transform.localScale;
-
-        Color color = playerImage.color;
-        color.a = 0f;
-        playerImage.color = color;
-        return playerImage;
     }
 }
