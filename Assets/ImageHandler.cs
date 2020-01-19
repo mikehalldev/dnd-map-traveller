ce89e4bca5dc735c5010680dcac2f4248583df8e
Michael Hall <mike@dynamometrics.com>
2020-01-19 12:40:55 -0500

    style: Format with C# FixFormat

--- Assets/ImageHandler.cs
+++ Assets/ImageHandler.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ImageHandler : MonoBehaviour
-{
+public class ImageHandler : MonoBehaviour {
 
     private Image[] images;
     private Canvas gmCanvas;
@@ -13,8 +12,7 @@ public class ImageHandler : MonoBehaviour
 
     private EventTrigger.Entry clickEntry;
 
-    void Start()
-    {
+    void Start() {
         gmCanvas = GameObject.Find("Game Manager").GetComponentInChildren<Canvas>();
         playerCanvas = GameObject.Find("Player").GetComponentInChildren<Canvas>();
 
@@ -24,13 +22,11 @@ public class ImageHandler : MonoBehaviour
         clickEntry.eventID = EventTriggerType.PointerClick;
         clickEntry.callback.AddListener((eventData) => { OnClick((PointerEventData) eventData); });
 
-        foreach (Image image in images)
-        {
+        foreach (Image image in images) {
             EventTrigger trigger = image.gameObject.AddComponent<EventTrigger>();
             trigger.triggers.Add(clickEntry);
 
-            Image playerImage = Instantiate(
-                image,
+            Image playerImage = Instantiate(image,
                 image.transform.position + playerCanvas.transform.position,
                 image.transform.rotation,
                 playerCanvas.transform
@@ -40,23 +36,18 @@ public class ImageHandler : MonoBehaviour
 
     }
 
-    private void OnClick(PointerEventData eventData)
-    {
+    private void OnClick(PointerEventData eventData) {
         Debug.Log(eventData.pointerPress.name);
         Image image = eventData.pointerPress.GetComponent<Image>();
         ChangeTransparency(image);
     }
 
-    private void ChangeTransparency(Image image)
-    {
+    private void ChangeTransparency(Image image) {
         Debug.Log("clicked!");
         Color color = image.color;
-        if (color.a == 1)
-        {
+        if (color.a == 1) {
             color.a = 0;
-        }
-        else
-        {
+        } else {
             color.a = 1;
         }
         image.color = color;
