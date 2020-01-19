ed91482d06064da52af24462790bf512515aeaeb
Michael Hall <mike@dynamometrics.com>
2020-01-19 12:55:58 -0500

    fix: Transparency passed to playerImage

--- Assets/ImageHandler.cs
+++ Assets/ImageHandler.cs
@@ -12,6 +12,8 @@ public class ImageHandler : MonoBehaviour {
 
     private EventTrigger.Entry clickEntry;
 
+    Dictionary<Image, Image> imagePairs;
+
     void Start() {
         gmCanvas = GameObject.Find("Game Manager").GetComponentInChildren<Canvas>();
         playerCanvas = GameObject.Find("Player").GetComponentInChildren<Canvas>();
@@ -22,28 +24,29 @@ public class ImageHandler : MonoBehaviour {
         clickEntry.eventID = EventTriggerType.PointerClick;
         clickEntry.callback.AddListener((eventData) => { OnClick((PointerEventData) eventData); });
 
+        imagePairs = new Dictionary<Image, Image>();
         foreach (Image image in images) {
             EventTrigger trigger = image.gameObject.AddComponent<EventTrigger>();
             trigger.triggers.Add(clickEntry);
 
-            Image playerImage = Instantiate(image,
+            Image playerImage = Instantiate(
+                image,
                 image.transform.position + playerCanvas.transform.position,
                 image.transform.rotation,
                 playerCanvas.transform
             );
             playerImage.transform.localScale = image.transform.localScale;
-        }
 
+            imagePairs.Add(image, playerImage);
+        }
     }
 
     private void OnClick(PointerEventData eventData) {
-        Debug.Log(eventData.pointerPress.name);
         Image image = eventData.pointerPress.GetComponent<Image>();
-        ChangeTransparency(image);
+        ChangeTransparency(imagePairs[image]);
     }
 
     private void ChangeTransparency(Image image) {
-        Debug.Log("clicked!");
         Color color = image.color;
         if (color.a == 1) {
             color.a = 0;
